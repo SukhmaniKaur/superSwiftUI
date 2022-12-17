62bd33e96249618254ee30b9bc8fdd5088c41972
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-12-17 22:04:51 +0530

    Initial Commit

---
+++ superSwiftUI/superSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  superSwiftUIApp.swift
+//  superSwiftUI
+//
+//  Created by Sukhmani Kaur on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct superSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

eba5ed503c03f15b2be812e5697c7f01bdf4855d
Sukhmani Kaur <sukhmani.ghataura@byjus.com>
2022-12-26 00:17:03 +0530

    implemented drag gesture on custom sign up action sheet

--- superSwiftUI/SignUpViewWithDragGesture.swift
+++ superSwiftUI/SignUpViewWithDragGesture.swift
@@ -8,36 +8,38 @@
 import SwiftUI
 
 struct SignUpViewWithDragGesture: View {
+    
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
+
     var body: some View {
         ZStack {
             Color.green.ignoresSafeArea()
             
-            VStack(spacing: 20) {
-                Image(systemName: "chevron.up")
-                Text("Sign up")
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                
-                Image(systemName: "flame.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                
-                Text("WebRTC(Web Real-Time Communication) is a set of technologies that is developed for peer to peer duplex real-time communication between browsers.")
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                
-                Text("Create an account")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .padding()
-                    .padding(.horizontal)
-                    .background(.black).cornerRadius(10)
-                Spacer()
-            }
-            .frame(maxWidth: .infinity)
-            .background(.white)
-            .cornerRadius(30)
+            MySignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring()) {
+                                currentDragOffsetY = value.translation.height
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                if currentDragOffsetY < -150 {
+                                    endingOffsetY = -startingOffsetY
+                                } else if endingOffsetY != 0 && currentDragOffsetY > 150 {
+                                    endingOffsetY = 0
+                                }
+                                currentDragOffsetY = 0
+                            }
+                        }
+                )
+            Text("\(currentDragOffsetY)")
         }
         .ignoresSafeArea(edges: .bottom)
     }
@@ -46,5 +48,37 @@ struct SignUpViewWithDragGesture: View {
 struct SignUpViewWithDragGesture_Previews: PreviewProvider {
     static var previews: some View {
         SignUpViewWithDragGesture()
+    }
+}
+
+struct MySignUpView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("WebRTC(Web Real-Time Communication) is a set of technologies that is developed for peer to peer duplex real-time communication between browsers.")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            Text("Create an account")
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(.black).cornerRadius(10)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .cornerRadius(30)
     }
 }
